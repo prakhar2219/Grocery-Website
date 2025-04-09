f7cfa2e97120979660af038fc2d9f72f18e5f9ac
prakhar2219 <welcomeprakhar@gmail.com>
2025-04-09 16:11:08 +0530

    categoies section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import MainBanner from '../component/MainBanner'
+import Categories from '../component/categories'
 function Home() {
   return (
     <div className='mt-10'>
       <MainBanner/>
+      <Categories/>
     </div>
   )
 }
