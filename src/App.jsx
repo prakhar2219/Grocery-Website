cf83f02f10c21a93f2e786458d4731ec271f47b6
prakhar2219 <welcomeprakhar@gmail.com>
2025-04-10 13:18:12 +0530

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from "./pages/Home.jsx"
 import { Toaster } from 'react-hot-toast'
 import Footer from './component/Footer.jsx'
+import { useAppContext } from './context/AppContext.jsx'
+import Login from './component/Login.jsx'
 const App = () => {
   const isSellerPath=useLocation().pathname.includes("seller")
+  const {showUserLogin}=useAppContext()
   return (
     <div>
      {isSellerPath ? null : <Navbar/>}
+{showUserLogin? <Login/>:null}
      <Toaster/>
       <div className={`${isSellerPath ? "" :"px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
