f5c15a51a64994319ed9ef213b1042e67871cd5d
prakhar2219 <welcomeprakhar@gmail.com>
2025-04-13 02:00:13 +0530

    User Logout

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,24 +1,43 @@
-import React, { use,useEffect } from "react";
+import React, { use, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
-  const {user,setUser,setShowUserLogin,navigate,searchQuery,setSearchQuery,getCartCount}=useAppContext();
+  const {
+    user,
+    setUser,
+    setShowUserLogin,
+    navigate,
+    searchQuery,
+    setSearchQuery,
+    getCartCount,
+    axios,
+  } = useAppContext();
 
-  const logout= async ()=>{
-    setUser(null);
-    navigate('/')
-  } 
+  const logout = async () => {
+    try {
+      const { data } = await axios.get("/api/user/logout");
+      if (data.success) {
+        toast.success(data.message);
+        setUser(null);
+        navigate("/");
+      }else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);}
+    navigate("/");
+  };
   useEffect(() => {
-if(searchQuery.length > 0){
-  navigate("/products")
-}
+    if (searchQuery.length > 0) {
+      navigate("/products");
+    }
   }, [searchQuery]);
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-      <NavLink to="/" onClick={()=>setOpen(false)}>
+      <NavLink to="/" onClick={() => setOpen(false)}>
         <img className="h-9" src={assets.logo} alt="logo" />
       </NavLink>
 
@@ -29,7 +48,8 @@ if(searchQuery.length > 0){
         <NavLink to="/">Contact</NavLink>
 
         <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-          <input onChange={(e) => setSearchQuery(e.target.value)}
+          <input
+            onChange={(e) => setSearchQuery(e.target.value)}
             className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500"
             type="text"
             placeholder="Search products"
@@ -37,7 +57,10 @@ if(searchQuery.length > 0){
           <img src={assets.search_icon} alt="search" className="w-4 h-4" />
         </div>
 
-        <div onClick={()=>navigate("/cart")} className="relative cursor-pointer">
+        <div
+          onClick={() => navigate("/cart")}
+          className="relative cursor-pointer"
+        >
           <img
             src={assets.nav_cart_icon}
             alt="cart"
@@ -48,21 +71,41 @@ if(searchQuery.length > 0){
           </button>
         </div>
 
-       {!user ? ( <button onClick={()=>setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
-          Login
-        </button>):(
-            <div className='relative group'>
-<img src={assets.profile_icon} className='w-10' alt=""/>
-<ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border borrder-gray-200 py-2.5 w-30 rounded-md 
-text-sm z-40'>
-    <li onClick={()=>navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
-    <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
-</ul>
-</div>
+        {!user ? (
+          <button
+            onClick={() => setShowUserLogin(true)}
+            className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full"
+          >
+            Login
+          </button>
+        ) : (
+          <div className="relative group">
+            <img src={assets.profile_icon} className="w-10" alt="" />
+            <ul
+              className="hidden group-hover:block absolute top-10 right-0 bg-white shadow border borrder-gray-200 py-2.5 w-30 rounded-md 
+text-sm z-40"
+            >
+              <li
+                onClick={() => navigate("my-orders")}
+                className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer"
+              >
+                My Orders
+              </li>
+              <li
+                onClick={logout}
+                className="p-1.5 pl-3 hover:bg-primary/10 cursor-pointer"
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
         )}
       </div>
-<div className="flex items-center gap-6 sm:hidden">
-<div onClick={()=>navigate("/cart")} className="relative cursor-pointer">
+      <div className="flex items-center gap-6 sm:hidden">
+        <div
+          onClick={() => navigate("/cart")}
+          className="relative cursor-pointer"
+        >
           <img
             src={assets.nav_cart_icon}
             alt="cart"
@@ -73,48 +116,58 @@ text-sm z-40'>
           </button>
         </div>
         <button
-        onClick={() => (open ? setOpen(false) : setOpen(true))}
-        aria-label="Menu"
-        className=""
-      >
-        {/* Menu Icon SVG */}
-        <img src={assets.menu_icon} alt="menu" />
-      </button>
-</div>
-
+          onClick={() => (open ? setOpen(false) : setOpen(true))}
+          aria-label="Menu"
+          className=""
+        >
+          {/* Menu Icon SVG */}
+          <img src={assets.menu_icon} alt="menu" />
+        </button>
+      </div>
 
       {/* Mobile Menu */}
-     {open && (
-         <div
-         className={`${
-           open ? "flex" : "hidden"
-         } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
-       >
-         <NavLink to="/" onClick={()=>setOpen(false)}>Home</NavLink>
-         <NavLink to="/products" onClick={()=>setOpen(false)}>All products</NavLink>
-         {/* user logged in then only we will show the message  */}
-         {user &&
-         <NavLink to="/" onClick={()=>setOpen(false)}>My Orders</NavLink>
-         }
-                 <NavLink to="/" onClick={()=>setOpen(false)}>Contact</NavLink>
- {/* user not logged in then login else logout */}
- {!user ?(
-  <button onClick={()=>{setOpen(false);
-     setShowUserLogin(true);
- 
-  }}
-   className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
-  Login
- </button>
- ):(
-     <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
-     Logout
-   </button>
- )
- }
-        
-       </div>
-     )}
+      {open && (
+        <div
+          className={`${
+            open ? "flex" : "hidden"
+          } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
+        >
+          <NavLink to="/" onClick={() => setOpen(false)}>
+            Home
+          </NavLink>
+          <NavLink to="/products" onClick={() => setOpen(false)}>
+            All products
+          </NavLink>
+          {/* user logged in then only we will show the message  */}
+          {user && (
+            <NavLink to="/" onClick={() => setOpen(false)}>
+              My Orders
+            </NavLink>
+          )}
+          <NavLink to="/" onClick={() => setOpen(false)}>
+            Contact
+          </NavLink>
+          {/* user not logged in then login else logout */}
+          {!user ? (
+            <button
+              onClick={() => {
+                setOpen(false);
+                setShowUserLogin(true);
+              }}
+              className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm"
+            >
+              Login
+            </button>
+          ) : (
+            <button
+              onClick={logout}
+              className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm"
+            >
+              Logout
+            </button>
+          )}
+        </div>
+      )}
     </nav>
   );
 };
