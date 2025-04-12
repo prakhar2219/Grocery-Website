02999ce2b65a2b015ba1a6aa4901729d5e1c2c85
prakhar2219 <welcomeprakhar@gmail.com>
2025-04-13 03:15:19 +0530

    order tracking by admin

--- src/pages/seller/Orders.jsx
+++ src/pages/seller/Orders.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { assets, dummyOrders } from '../../assets/assets';
 import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const Orders = () => {
-    const { currency } = useAppContext();
+    const { currency,axios } = useAppContext();
     const [orders,setOrders]=useState([])
     const fetchOrders=async()=>{
-        setOrders(dummyOrders)
+        try {
+           const {data}=await axios.get('/api/order/seller')
+              if(data.success){
+                setOrders(data.orders)}
+                else{
+                    toast.error(data.message)
+                } 
+        } catch (error) {
+            toast.error(error.message)
+        }
     }
     useEffect(()=>{
         fetchOrders()
